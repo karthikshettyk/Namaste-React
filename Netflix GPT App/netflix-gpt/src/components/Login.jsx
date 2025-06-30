6feb490b7663a30d538c0fc3b0d8c3c74bc02eb7
shetty-developer <shetty.developer@gmail.com>
2025-06-30 19:12:08 +0530

    second commit

--- Netflix GPT App/netflix-gpt/src/components/Login.jsx
+++ Netflix GPT App/netflix-gpt/src/components/Login.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./Header";
+import { validate } from "../utils/validate";
 
 const Login = () => {
+
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
+
+  const handleSubmit = () => {
+    const message = validate(
+      nameRef.current,
+      emailRef.current,
+      passwordRef.current,
+    );
+    setErrorMessage(message);
+  };
+
   return (
     <div>
       <Header></Header>
@@ -16,31 +32,43 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg"
           alt="background-img"
         />
-        <form className="w-full md:w-3/12 z-0 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="w-full md:w-3/12 z-0 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        >
           <h1 className="font-bold text-3xl py-4">
             {isSignInForm ? "Sign In" : "SignUp"}
           </h1>
           {!isSignInForm ? (
             <input
-              type="name"
+              type="text"
               placeholder="Full Name"
+              ref={nameRef}
               className="p-4 my-4 w-full bg-gray-700"
             ></input>
           ) : (
             <></>
           )}
           <input
-            type="name"
-            placeholder="Email or Mobile Number"
+            type="text"
+            placeholder="Email"
+            ref={emailRef}
             className="p-4 my-4 w-full bg-gray-700"
           ></input>
           <input
             type="password"
             placeholder="Password"
+            ref={passwordRef}
             className="p-4 my-4 w-full bg-gray-700"
           ></input>
-          <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-         {isSignInForm?"Login":"Sign Up"}
+          <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+          <button
+            className="p-4 my-6 bg-red-700 w-full rounded-lg"
+            onClick={handleSubmit}
+          >
+            {isSignInForm ? "Login" : "Sign Up"}
           </button>
           <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
             {isSignInForm
@@ -51,6 +79,7 @@ const Login = () => {
       </div>
     </div>
   );
+
 };
 
 export default Login;
