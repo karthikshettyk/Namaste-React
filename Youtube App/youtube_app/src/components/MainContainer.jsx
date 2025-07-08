8b6576a1828bd8a5d2be5e3caaa438e93ff1b013
shetty-developer <shetty.developer@gmail.com>
2025-07-08 17:59:22 +0530

    youtube head part is completed

---
+++ Youtube App/youtube_app/src/components/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ButtonList from "./ButtonList";
+import VideoContainer from "./VideoContainer";
+
+const MainContainer = () => {
+  return (
+    <>
+      <div>MainContainer</div>
+      <ButtonList></ButtonList>
+      <VideoContainer></VideoContainer>
+    </>
+  );
+};
+
+export default MainContainer;
